3e64f7f2ae4bbea572725a3fb231b3aa4035c5db
andrey rojas <andreymrojas668@gmail.com>
2022-07-02 08:37:30 -0500

    mi segundo ejemplo

---
+++ src/segundoejemplo.java
@@ -0,0 +1,14 @@
+public class segundoejemplo {
+    public static void main(String[]args){
+        // escribir los numeros del 10 al 1
+        int numero = 10;
+        while (numero >=1){
+            System.out.println("numero:"+numero);
+            //numero--;
+            numero=numero-1;
+
+
+
+        }
+    }
+}
