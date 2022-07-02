90bbb11b9a458ccb016e76de76792683423071ec
andrey rojas <andreymrojas668@gmail.com>
2022-07-02 09:03:26 -0500

    ejercicio 1

---
+++ src/ejercicio1.java
@@ -0,0 +1,17 @@
+public class ejercicio1 {
+    public static void maid(String[] args) {
+        int numero = 1;
+        string letra;
+        while (numero < 100) {
+            if (numero % 2 == 0) {
+                letra = "l";
+                else{
+                    letra "D";
+                }
+                System.out println (numero + "-" + letra);
+                numero = numero + 1;
+
+            }
+        }
+    }
+}
